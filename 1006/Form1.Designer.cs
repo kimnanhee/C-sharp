3e0f7c8f9ac63197183f6e7cd0bfb0adca47ac3d
kimnanhee <kimnanhee0225@gmail.com>
2020-10-06 19:59:40 +0900

    [Add] 1006, README

---
+++ 1006/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace _1006
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(276, 152);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        void make_arr(int count)
+        {
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2 = new System.Windows.Forms.PictureBox[count];
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            for (int i = 0; i < count; i++)
+            {
+                this.pictureBox2[i] = new System.Windows.Forms.PictureBox();
+                ((System.ComponentModel.ISupportInitialize)(this.pictureBox2[i])).BeginInit();
+                this.pictureBox2[i].Image = global::_1006.Properties.Resources.사과;
+                this.pictureBox2[i].Location = new System.Drawing.Point(30+(60*i), 30);
+                this.pictureBox2[i].Name = "pictureBox2";
+                this.pictureBox2[i].Size = new System.Drawing.Size(50, 60);
+                this.pictureBox2[i].SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+                this.pictureBox2[i].TabIndex = 2;
+                this.pictureBox2[i].TabStop = false;
+                ((System.ComponentModel.ISupportInitialize)(this.pictureBox2[i])).EndInit();
+                this.Controls.Add(this.pictureBox2[i]);
+            }
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox[] pictureBox2;
+    }
+}
+
---
+++ 1006/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1006
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            make_arr(10); // 10개 배열 만들기
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e) // 클릭한 위치로 이동
+        {
+            pictureBox1.Left = e.X;
+            pictureBox1.Top = e.Y;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) // 눌린 키 값에 따라 이동
+        {
+            if (e.KeyData == Keys.Left) pictureBox1.Left -= 10;
+            else if (e.KeyData == Keys.Right) pictureBox1.Left += 10;
+            else if (e.KeyData == Keys.Up) pictureBox1.Top -= 10;
+            else if (e.KeyData == Keys.Down) pictureBox1.Top += 10;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // 0.1초마다 실행
+        {
+            for(int i=0; i<10; i++) // 10개 움직이기
+            {
+                pictureBox2[i].Top += 10;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
