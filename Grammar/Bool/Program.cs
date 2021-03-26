75ee46bd858c6def7b36817edda50f57ebc3b760
kimnanhee <kimnanhee0225@gmail.com>
2021-03-26 11:37:46 +0900

    [Add] grammar bool, int, float type'

---
+++ Grammar/Bool/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            int c = 123;
+            object d = (object)c; // 값을 박싱해서 힙에 저장
+            int e = (int)d; // 값을 언박싱해서 스택에 저장
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+
+            float f = 3.14159265358979323846264338f;
+            double g = 3.14159265358979323846264338;
+            decimal h = 3.14159265358979323846264338m;
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+        }
+    }
+}
---
+++ Grammar/Test_code/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, {0} {1} {2}", args[0], args[1], args[2]);
+        }
+    }
+}
