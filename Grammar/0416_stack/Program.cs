092a9e6bf8fb2e42e55e93b7c2b4bce1e3b365af
kimnanhee <kimnanhee0225@gmail.com>
2021-04-23 23:27:19 +0900

    [Add] 0416 stack folder

---
+++ Grammar/0416_stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0416_stack
+{
+    class Stack
+    {
+        static void push(object arg)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
