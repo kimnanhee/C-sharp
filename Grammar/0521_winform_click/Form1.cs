ae5efd7c9c23beae9fe2647bc4f384aa1e6df69d
kimnanhee <kimnanhee0225@gmail.com>
2021-05-21 16:09:39 +0900

    [Add] 0521 winform click event

---
+++ Grammar/0521_winform_click/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Random rand;
+        public Form1()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MouseWheel += new MouseEventHandler(Form1_MouseWheel);
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left) // 왼쪽 클릭
+            {
+                Color oldColor = this.BackColor;
+                this.BackColor = Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+            }
+            else if(e.Button==MouseButtons.Right) // 오른쪽 클릭
+            {
+                if(this.BackgroundImage != null)
+                {
+                    this.BackgroundImage = null;
+                }
+                else
+                {
+                    string fname = "sample.jpg";
+                    if (System.IO.File.Exists(fname) == false)
+                        Console.WriteLine("이미지 파일이 없습니다.");
+                    else
+                        this.BackgroundImage = Image.FromFile(fname);
+                }
+            }
+        }
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            this.Opacity = this.Opacity + (e.Delta > 0 ? 0.1 : -0.1);
+            if (this.Opacity <= 0.1) this.Opacity = 0.1;
+            Console.WriteLine("Opacity : {0}", this.Opacity);
+        }
+    }
+}
---
+++ Grammar/0521_winform_click/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Start Form");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            Console.WriteLine("End Form");
+        }
+    }
+}
